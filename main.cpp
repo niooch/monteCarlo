1d3cb0e5c19b3b42684f64818c4cd58737eead7b
Jakub Kogut <jakub@jkogut.pl>
2024-11-06 20:42:13 +0100

    finito

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 #include <cmath>
 #include <random>
 #include <vector>
-#include "header.cpp"
+#include <string>
+// Function declarations (assuming they are in "header.cpp" or defined elsewhere)
+double monteCarloCalka(double (*f)(double), double a, double b, double M, int n);
+double f1(double x);
+double f2(double x);
+double f3(double x);
+double f_pi(double x);
+
 
 double (*wybierzFunkcje(const std::string &typ, double &a, double &b, double &M, double &dokladnaWartosc)) (double) {
     if(typ == "f1"){
@@ -35,18 +42,18 @@ double (*wybierzFunkcje(const std::string &typ, double &a, double &b, double &M,
         dokladnaWartosc=M_PI;
         return f_pi;
     } else {
-        std::cerr<<"nie prawidlowe uzycie: f1, f2, f3 lub pi."<<std::endl;
+        std::cerr<<"Nieprawidłowe użycie: f1, f2, f3 lub pi."<<std::endl;
         exit(1);
     }
 }
 
 int main(int argc, char* argv[]){
     if(argc<4){
-        std::cerr<<"Uzycie: k, n, funkcja"<<std::endl;
+        std::cerr<<"Użycie: k, n, funkcja"<<std::endl;
         return 1;
     }
 
-    //przyjecie argumentow
+    // Przyjęcie argumentów
     int k = std::stoi(argv[1]);
     int n = std::stoi(argv[2]);
     std::string typ = argv[3];
@@ -54,26 +61,26 @@ int main(int argc, char* argv[]){
     double a, b, M, dokladnaWartosc;
     double (*f)(double) = wybierzFunkcje(typ, a, b, M, dokladnaWartosc);
 
-    std::vector<double> wyniki;
-    for(int i=0; i<k; i++){
-        double calka = monteCarloCalka(f, a, b, M, n);
-        wyniki.push_back(calka);
-        //wynik do gnuplota
-        std::cout<<n<<"     "<<calka<<std::endl;
-    }
+    std::vector<double> wyniki(k);
 
-    //liczenie sredniej
+    for(int i=0; i<k; i++){
+            double calka = monteCarloCalka(f, a, b, M, n);
+            wyniki[i] = calka;
+                std::cout << n << "     " << calka << std::endl;
+        }
+
+    // Liczenie średniej
     double srednia=0.0;
     for(auto & i : wyniki)
         srednia+=i;
     srednia/=wyniki.size();
-    std::cout<<n<<"      "<<srednia<<"       "<<dokladnaWartosc<<std::endl;
+    std::cout << n << " " << srednia << " " << dokladnaWartosc << std::endl;
 
     return 0;
 }
 
 double monteCarloCalka(double (*f)(double), double a, double b, double M, int n){
-    //przyszykowanie maszyny losujacej:
+    // Przygotowanie maszyny losującej:
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> x_dist(a, b);
@@ -86,12 +93,12 @@ double monteCarloCalka(double (*f)(double), double a, double b, double M, int n)
         if(y<=f(x))
             C++;
     }
-    //z polecenia
+    // Z polecenia
     double pole=(b-a)*M;
     return (static_cast<double>(C)/n)*pole;
 }
 
-//definicje funkcji:
+// Definicje funkcji:
 double f1(double x){
     return std::pow(x, 1.0/3.0);
 }
